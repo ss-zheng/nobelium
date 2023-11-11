342f379266e0fee6503f25d66f7174be371b21e0
Daniel Zheng <danielzheng1997@gmail.com>
2023-11-11 01:35:20 -0800

    enabled google analytics, also add social media footer

--- blog.config.js
+++ blog.config.js
@@ -11,14 +11,17 @@ const BLOG = {
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
-  since: 2023, // If leave this empty, current year will be used.
+  since: 2022, // If leave this empty, current year will be used.
   postsPerPage: 7,
   sortByDate: true,
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/zssdan',
+  socialLink: 'https://twitter.com/zssdan/',
+  xlink: 'https://twitter.com/zssdan/',
+  linkedin: 'https://www.linkedin.com/in/danzss/',
+  github: 'https://github.com/ss-zheng',
   seo: {
     keywords: ['Blog', 'AI', 'Metaverse'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
@@ -26,14 +29,14 @@ const BLOG = {
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   analytics: {
-    provider: '', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
+    provider: 'ga', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
     ackeeConfig: {
       tracker: '', // e.g 'https://ackee.craigary.net/tracker.js'
       dataAckeeServer: '', // e.g https://ackee.craigary.net , don't end with a slash
       domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: '' // e.g: G-XXXXXXXXXX
+      measurementId: 'G-7KM5ZX2FE5' // e.g: G-XXXXXXXXXX
     }
   },
   comment: {
