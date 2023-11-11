d7038ac557c61110cc8ecc1df974b78f5cd272f0
Daniel Zheng <danielzheng1997@gmail.com>
2023-11-10 21:18:56 -0800

    try with vercel

--- next.config.js
+++ next.config.js
@@ -1,24 +1,23 @@
 module.exports = {
-  output: 'export',
   images: {
     domains: ['gravatar.com']
   },
   eslint: {
     // dirs: ['components', 'layouts', 'lib', 'pages']
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       source: '/:path*{/}?',
-  //       headers: [
-  //         {
-  //           key: 'Permissions-Policy',
-  //           value: 'interest-cohort=()'
-  //         }
-  //       ]
-  //     }
-  //   ]
-  // },
+  async headers() {
+    return [
+      {
+        source: '/:path*{/}?',
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            value: 'interest-cohort=()'
+          }
+        ]
+      }
+    ]
+  },
   transpilePackages: ['dayjs']
   // webpack: (config, { dev, isServer }) => {
   //   // Replace React with Preact only in client production build
